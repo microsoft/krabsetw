e45939238aed641a7e3fc3ae5941f5ccc47e5649
Zac Brown <zacbrown@users.noreply.github.com>
2017-10-17 15:55:47 -0700

    Update sign.cmd.

--- scripts/sign.cmd
+++ scripts/sign.cmd
@@ -1,7 +1,7 @@
-msbuild krabs\krabs.sln /t:Rebuild /property:Configuration=DebugSigning;Platform="x64" /m:8 /nologo
-msbuild krabs\krabs.sln /t:Rebuild /property:Configuration=ReleaseSigning;Platform="x64" /m:8 /nologo
-sn -R krabs\x64\DebugSigning\O365.Security.Native.ETW.dll e:\certs\new-code-signing\O365.Security.pfx
-sn -R krabs\x64\ReleaseSigning\O365.Security.Native.ETW.dll e:\certs\new-code-signing\O365.Security.pfx
-nuget pack O365.Security.Native.ETW.nuspec
-nuget pack O365.Security.Native.ETW.Debug.nuspec
-nuget pack krabsetw.nuspec
+msbuild %~dp0..\krabs\krabs.sln /t:Rebuild /property:Configuration=DebugSigning;Platform="x64" /m:8 /nologo
+msbuild %~dp0..\krabs\krabs.sln /t:Rebuild /property:Configuration=ReleaseSigning;Platform="x64" /m:8 /nologo
+sn -R %~dp0..\krabs\x64\DebugSigning\O365.Security.Native.ETW.dll  %~dp0\O365.Security.pfx
+sn -R %~dp0..\krabs\x64\ReleaseSigning\O365.Security.Native.ETW.dll  %~dp0\O365.Security.pfx
+nuget pack  %~dp0..\O365.Security.Native.ETW.nuspec
+nuget pack  %~dp0..\O365.Security.Native.ETW.Debug.nuspec
+nuget pack  %~dp0..\krabsetw.nuspec
