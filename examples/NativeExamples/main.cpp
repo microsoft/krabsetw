8a33b431f02f48fbbf65b4b29b3d4370d76c563c
Issahar Noam Gourfinkel <issaharg@cynet.com>
2018-11-01 09:09:42 +0200

    resolving issue 1 in comments of pull request

--- examples/NativeExamples/main.cpp
+++ examples/NativeExamples/main.cpp
@@ -6,11 +6,11 @@
 int main(void)
 {
     // Comment in/out the particular example you'd like to run.
-    //kernel_and_user_trace_001::start();
+    kernel_and_user_trace_001::start();
     //kernel_trace_001::start();
     //multiple_providers_001::start();
     //testing_001::start();
     //user_trace_001::start();
     //user_trace_002::start();
-	user_trace_003_no_predicates::start();
+	//user_trace_003_no_predicates::start();
 }
