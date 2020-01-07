6a0f093f77f971cb05d99d1cf364e92208dc4223
John U <53329154+jdu2600@users.noreply.github.com>
2020-01-08 08:53:52 +1100

    updated exception to reflect decoupling of open() and process() (#91)

--- Microsoft.O365.Security.Native.ETW/KernelTrace.hpp
+++ Microsoft.O365.Security.Native.ETW/KernelTrace.hpp
@@ -147,9 +147,9 @@ namespace Microsoft { namespace O365 { namespace Security { namespace ETW {
         {
             throw gcnew InvalidParameter;
         }
-        catch (const krabs::start_trace_failure &)
+        catch (const krabs::open_trace_failure &)
         {
-            throw gcnew StartTraceFailure;
+            throw gcnew OpenTraceFailure;
         }
         catch (const krabs::no_trace_sessions_remaining &)
         {
