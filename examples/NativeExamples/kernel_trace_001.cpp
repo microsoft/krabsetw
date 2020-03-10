8bdbd5e9aa07350112cc98f14fe1aa4319a91095
John U <53329154+jdu2600@users.noreply.github.com>
2020-03-10 22:31:58 +1100

    added PERFINFO_GROUPMASK examples

--- examples/NativeExamples/kernel_trace_001.cpp
+++ examples/NativeExamples/kernel_trace_001.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include "..\..\krabs\krabs.hpp"
+#include "..\..\krabs\krabs\perfinfo_groupmask.hpp"
 #include "examples.h"
 
 void kernel_trace_001::start()
@@ -18,7 +19,7 @@ void kernel_trace_001::start()
     // providers that are allowed by kernel traces is hardcoded by Windows ETW, and
     // Krabs provides simple objects to represent these. If other providers were
     // enabled without krabs being updated, the same thing could be done like so:
-    //    krabs::kernel_provider provider(SOME_ULONG_VALUE, SOME_GUID);
+    //    krabs::kernel_provider provider(SOME_ULONG_FLAG_VALUE, SOME_GUID);
     krabs::kernel::image_load_provider provider;
 
     // Kernel providers accept all the typical callback mechanisms.
@@ -38,8 +39,25 @@ void kernel_trace_001::start()
         }
     });
 
+    // Some kernel providers can't be enabled via EnableFlags and you need to call
+    // TraceSetInformation with an extended PERFINFO_GROUPMASK instead.
+    // e.g. https://docs.microsoft.com/en-us/windows/win32/etw/obtrace
+    // Krabs has convenience providers for some of these, but otherwise the same
+    // thing could be done with:
+    //    krabs::kernel_provider provider(SOME_GUID, SOME_ULONG_MASK_VALUE);
+    krabs::kernel::object_manager_provider ob_provider;
+    ob_provider.add_on_event_callback([](const EVENT_RECORD& record, const krabs::trace_context& trace_context) {
+        if (record.EventHeader.EventDescriptor.Opcode == 33) {
+            krabs::schema schema(record, trace_context.schema_locator);
+            krabs::parser parser(schema);
+            std::wstring name = parser.parse<std::wstring>(L"ObjectName");
+            std::wcout << L"Handle closed for object with name " << name << std::endl;
+        }
+    });
+
     // From here on out, a kernel_trace is indistinguishable from a user_trace in how
     // it is used.
     trace.enable(provider);
+    trace.enable(ob_provider);
     trace.start();
 }
