f7acd9a648ce30d4888137f43b9cf7267b43b88e
pskhodad <prashant.s.khodade@gmail.com>
2020-04-08 06:07:19 +0530

    Update schema to support TraceLogging events (#119)
    * Support TaskName - necessary for processing TraceLogging events
    
    1) TraceLogging events can be distinguished based on TaskName. Add support for retrieving TaskName.
    2) Client can check DECODING_SOURCE and identify if provider is using TraceLogging API.  If DecodingSource is DecodingSourceTlg then its TraceLogging provider event.
    
    * Update version number and release notes in .nuspec
    
    Update version number and release notes in .nuspec

--- krabs/krabs/schema.hpp
+++ krabs/krabs/schema.hpp
@@ -80,6 +80,34 @@ namespace krabs {
          */
         const wchar_t *event_name() const;
 
+        /*
+         * <summary>
+         * Returns the taskname of an event via its schema.
+         * </summary>
+         * <example>
+         *    void on_event(const EVENT_RECORD &record, const krabs::trace_context &trace_context)
+         *    {
+         *        krabs::schema schema(record, trace_context.schema_locator);
+         *        std::wstring name = krabs::task_name(schema);
+         *    }
+         * </example>
+         */
+        const wchar_t *task_name() const;
+
+        /*
+         * <summary>
+         * Returns the DECODING_SOURCE of an event via its schema.
+         * </summary>
+         * <example>
+         *    void on_event(const EVENT_RECORD &record, const krabs::trace_context &trace_context)
+         *    {
+         *        krabs::schema schema(record, trace_context.schema_locator);
+         *        DECODING_SOURCE source = krabs::decoding_source(schema);
+         *    }
+         * </example>
+         */
+        DECODING_SOURCE decoding_source() const;
+
         /**
          * <summary>
          * Returns the event ID via its schema.
@@ -184,6 +212,8 @@ namespace krabs {
 
     private:
         friend std::wstring event_name(const schema &);
+        friend std::wstring task_name(const schema &);
+        friend DECODING_SOURCE decoding_source(const schema &);
         friend std::wstring provider_name(const schema &);
         friend unsigned int process_id(const schema &);
         friend LARGE_INTEGER timestamp(const schema &);
@@ -221,6 +251,18 @@ namespace krabs {
         return reinterpret_cast<const wchar_t*>(
             reinterpret_cast<const char*>(pSchema_) +
             pSchema_->OpcodeNameOffset);
+    }
+
+    inline const wchar_t *schema::task_name() const
+    {
+        return reinterpret_cast<const wchar_t*>(
+            reinterpret_cast<const char*>(pSchema_) +
+            pSchema_->TaskNameOffset);
+    }
+
+    inline DECODING_SOURCE schema::decoding_source() const
+    {
+        return pSchema_->DecodingSource;
     }
 
     inline int schema::event_id() const
