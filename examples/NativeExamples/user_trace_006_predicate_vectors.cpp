c18da4f181e6deef89802e14319756eb204f4db8
pathtofile <path.to.file@gmail.com>
2020-04-14 22:16:17 +1000

    fixed formatting in user_trace_006_predicate_vectors.cpp

--- examples/NativeExamples/user_trace_006_predicate_vectors.cpp
+++ examples/NativeExamples/user_trace_006_predicate_vectors.cpp
@@ -32,8 +32,8 @@ void user_trace_006_predicate_vectors::start()
             &opcode_is_1,
             &eventid_is_2,
             &version_is_3,
-            })
-            );
+        })
+    );
     filter.add_on_event_callback([](const EVENT_RECORD& record, const krabs::trace_context& trace_context) {
         krabs::schema schema(record, trace_context.schema_locator);
         assert(schema.event_id() == 1 || schema.event_id() == 2);
